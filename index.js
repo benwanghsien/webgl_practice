5e0dd1507849df894cfd4b21894ea4d92e90d121
Tgo Wang <ben2021345@hotmail.com>
2022-12-31 20:44:39 +0800

    fill the canvas with color

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const canvas = document.getElementById("canvas");
 const gl = canvas.getContext("webgl");
 window.gl = gl;
+
+// Fill the canvas with color, actually clear the color
+gl.clearColor(120, 0, 150, 1); // set the color, syntax: clearColor(red, green, blue, alpha) RGBA
+gl.clear(gl.COLOR_BUFFER_BIT); // clear the canvas, gl.COLOR_BUFFER_BIT means the parts want to be cleared
